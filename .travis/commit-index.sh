091059d752d2858e4e47b43681acbdfe5af21f36
Tomasz Nocoń <tnocon@novomatic-tech.com>
2019-05-06 17:27:06 +0200

    Add authentication for git pushes

--- .travis/commit-index.sh
+++ .travis/commit-index.sh
@@ -6,4 +6,5 @@ git add index.yaml
 git config user.name  "Travis"
 git config user.email "travis@travis-ci.org"
 git commit -m "Update index.yaml file."
+git config credential.helper $GITHUB_TOKEN
 git push origin $TRAVIS_BRANCH 2>&1 > /dev/null
