155b6616c50ad0ebbad5e559a5dbc29836ab0472
Tomasz Nocoń <thom.nocon@gmail.com>
2019-05-06 17:39:37 +0200

    Fix publish script

--- .travis/commit-index.sh
+++ .travis/commit-index.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+PUBLICATION_BRANCH=$TRAVIS_BRANCH
+PUBLICATION_REPO=novomatic-tech/helm-charts-repo
+PUBLICATION_FILE=index.yaml
+
+git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/$PUBLICATION_REPO publish 2>&1 > /dev/null
+cp -f $PUBLICATION_FILE publish
+cd publish
 git add index.yaml
 git config user.name  "Travis"
 git config user.email "travis@travis-ci.org"
 git commit -m "Update index.yaml file."
-git config credential.helper $GITHUB_TOKEN
-git push origin $TRAVIS_BRANCH 2>&1 > /dev/null
+git push origin $PUBLICATION_BRANCH 2>&1 > /dev/null
